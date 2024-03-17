cf2058b1e66ce9d67bedf568bdfc1de6cfa5931d
Shigeki TORII <torichanjp@gmail.com>
2024-03-17 15:25:15 +0900

    トークンシンボルが「AsoUbuyama」に変わらない問題

--- packages/nouns-contracts/contracts/NounsDescriptor.sol
+++ packages/nouns-contracts/contracts/NounsDescriptor.sol
@@ -261,8 +261,8 @@ contract NounsDescriptor is INounsDescriptor, Ownable {
      */
     function dataURI(uint256 tokenId, INounsSeeder.Seed memory seed) public view override returns (string memory) {
         string memory nounId = tokenId.toString();
-        string memory name = string(abi.encodePacked('Noun ', nounId));
-        string memory description = string(abi.encodePacked('Noun ', nounId, ' is a member of the Nouns DAO'));
+        string memory name = string(abi.encodePacked('AsoUbuyama Noun ', nounId));
+        string memory description = string(abi.encodePacked('AsoUbuyama Noun ', nounId, ' is a member of the AsoUbuyama Nouns DAO'));
 
         return genericDataURI(name, description, seed);
     }
